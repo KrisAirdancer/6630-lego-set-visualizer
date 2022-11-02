7b2f07ac9500f9c70512c94190c2b695fdb47b66
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-02 17:26:48 -0600

    Setup the basic project files.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Load data with dataProcess.js
+// Initialize visualization objects
+    // One for each vis
+    // Pass each one a copy of the data object
+    // One class (.js file) for each visualization
+// Call the drawX() functions for each of the visualizations
+
+console.log("Hello, World!");
