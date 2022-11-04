e7be9666637cae12b32d711d4724451ac83ae7c5
Vista Marston <63620520+VistaMarston@users.noreply.github.com>
2022-11-04 15:30:05 -0600

    Update theSquiggler.js
    Outline the Connected Scatter plot graph

--- js/theSquiggler.js
+++ js/theSquiggler.js
@@ -1,5 +1,39 @@
 // Connected scatterplot - The Squiggler
 
+/**
+ * This class is used to create the connected scatterplot
+ * to visualization the average number of color per set by 
+ * the total number of themes per year
+ */
 class theSquiggler {
+
+    /**
+     * A constructor to create a squiggler object
+     * @param {Object} globalData 
+     */
+    constructor(globalData) {
+        // Create an XScale -> num of themes
+        // Create an YScale -> average color/set
+        
+    }
+
+    /**
+     * A helper function that will draw the 
+     * connected scatter plot for the visualizaiton
+     * @param {Object} data 
+     */
+    drawlineChart(data) {
+        // Draw a path from each data
+        // .on tooltip that give the information
+    }
+
+    /**
+     * A helper function that will be used to highlight some aspect
+     * of the data
+     */
+    showStoryComponent() {
+
+    }
+
 
 }
