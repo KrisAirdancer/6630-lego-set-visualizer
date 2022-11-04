2b58c26eb19d978893be91ef653eb2147ef2adc3
Vista Marston <63620520+VistaMarston@users.noreply.github.com>
2022-11-04 13:52:27 -0600

    Update script.js
    Finished processing the data. It is broken down into year, set_num, set_name, num_parts, theme_name, num_color.

--- js/script.js
+++ js/script.js
@@ -5,39 +5,98 @@
     // One class (.js file) for each visualization
 // Call the drawX() functions for each of the visualizations
 
-data_list = [d3.csv('./data/colors.csv'),
-            d3.csv('./data/elements.csv'),
-            d3.csv('./data/inventories.csv'),
-            d3.csv('./data/inventory_minifigs.csv'),
-            d3.csv('./data/inventory_parts.csv'),
-            d3.csv('./data/inventory_sets.csv'),
-            d3.csv('./data/minifigs.csv'),
-            d3.csv('./data/part_categories.csv'),
-            d3.csv('./data/part_relationships.csv'),
-            d3.csv('./data/parts.csv'),
-            d3.csv('./data/sets.csv'),
-            d3.csv('./data/themes.csv')];
+data_list = [
+            d3.csv('./data/inventories.csv'), // used
+            d3.csv('./data/inventory_parts.csv'), // used
+            d3.csv('./data/parts.csv'), //used
+            d3.csv('./data/colors.csv'), // used
+            d3.csv('./data/inventory_sets.csv'), // used
+            d3.csv('./data/sets.csv'), // used
+            d3.csv('./data/themes.csv') ]; // used
 
 Promise.all(data_list).then(data => 
     {
-        // Processing the data
-        console.log(data);
-            
+        data = processData(data);
     }
 );
 
-console.log("Hello, World!");
 
-let data = processData()
-console.log(data)
+/**
+ * Processes the data to only include only the 
+ * informaiton that is nesscary for the visualizations.
+ * @param {Array} data 
+ * @returns Processed Data
+ */
+function processData(data) {
+    let years = acendingYearData(data);
+    let parts = d3.group(data[1], d => d.inventory_id);
+    let group_inv = d3.group(data[0], d => d.set_num);
+    
+    // Create a data structure to store the information
+    let processData = [];
+    let pIndex = 0;
 
-
-
-
-
-function processData() {
-    return {
-        ID: 007,
-        name: "James Bond"
+    // Adding the data to the years
+    for( let [key, value] of years) {
+        for (let index = 0; index < value.length; index++) {
+            processData[pIndex++] = {
+                year: value[index].year,
+                set_name: value[index].name,
+                theme_name: getTheme_id(data[6], value[index]),
+                num_parts: parseInt(value[index].num_parts),
+                num_color: getNum_Colors(parts, group_inv, data[4], value[index].set_num)
+            };
+        }
     }
+
+    return processData;
+}
+
+/**
+ * A helper function that will sort the data based on years in
+ * Acending Order 
+ * @param {Array} data 
+ * @returns data grouped by year in acending order
+ */
+function acendingYearData(data) {
+    // Sort the data based on the year in acending 
+    let sorted_sets = data[5].sort((a,b) => {
+        return (a.year > b.year)? 1: -1;
+    });
+
+    return d3.group(sorted_sets, d => d.year);
+}
+
+/**
+ * A helper function that will pull all of the 
+ * @param {Array} themes 
+ * @param {Object} set 
+ * @returns Theme name associated with the set
+ */
+function getTheme_id(themes, set) {
+    let theme_index = themes.findIndex(d => d.id == set.theme_id);
+    return themes[theme_index].name;
+}
+
+/**
+ * A helper function that will count the number of 
+ * unique colors within each set.
+ * @param {Array} colors 
+ * @param {Object} set 
+ */
+function getNum_Colors(inv_part, inv, inv_set, set_num) {
+    let inv_id = inv.get(set_num);
+    
+    let unique_colors = new Set();
+    for(let index = 0; index < inv_id.length; index++) {
+        let value = inv_id[index];
+        let parts = inv_part.get(value.id);
+        if(parts !== undefined) {
+            let colors = d3.group(parts, d => d.color_id);
+            for([key, value] of colors) {
+                unique_colors.add(colors.get(key));
+            }
+        }
+    }
+    return unique_colors.size;
 }
