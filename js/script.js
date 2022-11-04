4d8e48a5eb4febb81ebdbc9fe5598d23a42e2a46
Vista Marston <63620520+VistaMarston@users.noreply.github.com>
2022-11-04 08:12:13 -0600

    Processing Data
    Loaded the data into script data, not processed into an easier data format

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@
     // One class (.js file) for each visualization
 // Call the drawX() functions for each of the visualizations
 
+data_list = [d3.csv('./data/colors.csv'),
+            d3.csv('./data/elements.csv'),
+            d3.csv('./data/inventories.csv'),
+            d3.csv('./data/inventory_minifigs.csv'),
+            d3.csv('./data/inventory_parts.csv'),
+            d3.csv('./data/inventory_sets.csv'),
+            d3.csv('./data/minifigs.csv'),
+            d3.csv('./data/part_categories.csv'),
+            d3.csv('./data/part_relationships.csv'),
+            d3.csv('./data/parts.csv'),
+            d3.csv('./data/sets.csv'),
+            d3.csv('./data/themes.csv')];
+
+Promise.all(data_list).then(data => 
+    {
+        // Processing the data
+        console.log(data);
+            
+    }
+);
 
 console.log("Hello, World!");
 
