68f8c2e3c471bd4cd5c6c78c2243a552edf58dd7
VistaMarston <63620520+VistaMarston@users.noreply.github.com>
2022-11-05 14:24:26 -0600

    Fixed the data processing
    Fixed the tooltip for the heat map. In addition, filtered out all of the data doesn't have any pieces.

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,4 +1,5 @@
 // Number of colors per set per year
+
 /**
  * This class is used to visualize the color over the total number of sets per
  * year, which is visualized using a heat map
@@ -10,8 +11,9 @@ class Heatmap {
      * @param {Array Object} globalData 
      */
     constructor(data) {
-        data = data.filter(d => d.num_parts >= 2500);
+        this.completeData = data;
         console.log(data);
+        data = data.filter(d => d.num_parts >= 2500);
 
         this.padding = {
             top: 10,
@@ -20,127 +22,207 @@ class Heatmap {
             left: 50
         };
 
+        this.updateHeatmap(data)
+
+        this.createToolkit();
+    }
+
+    //#region Setup
+    /**
+     * A helper function that will create all of the scalling for
+     * the heatmap visualization.
+     * @param {Objecy} data 
+     * @param {int} height 
+     * @param {int} width 
+     */
+    createScaling(data, height, width, svg) {
         let yearData = [...d3.group(data, d => d.year).keys()];
         let setName = [...d3.group(data, d=> d.set_name).keys()];
         setName = setName.sort((a,b) => (a>b)?-1:1);
 
-        let svg = d3.select("#svg_heatmap").attr("height", 700);
-        svg.append("g").attr("id", "heat_tool_tip");
+        this.createXAxis(yearData, width, height, svg);
+        this.createYAxis(setName, height);
+        this.createColorScale(data);   
+    }
 
-        let height = parseInt(svg.style("height"))-this.padding.bottom;
-        let width = parseInt(svg.style("width")) - this.padding.right;
-        
+    /**
+     * A helper function for creating the color scale 
+     * for the heatmap
+     * @param {Object} data 
+     */
+    createColorScale(data) {
+        let max_color = parseInt(d3.max(data, d=> d.num_color));
+        let min_color = parseInt(d3.min(data, d=> d.num_color))
+
+        this.colorScale = d3.scaleLinear()
+            .domain([min_color, max_color])
+            .range(['white', "#F9A900"]);
+    }
+
+    /**
+     * A helper function that will create
+     * the YScale Axis for the Heatmap graph
+     * @param {Object} setName 
+     * @param {Integer} height 
+     */
+    createYAxis(setName, height) {
+        // Create Y-Scale
+        this.yScale = d3.scaleBand()
+            .domain(setName)
+            .range([this.padding.top, height - this.padding.bottom])
+            .padding(.01);
+    }
+
+    /**
+     * A helper function that will draw the x-axis
+     * on the heat map
+     * @param {Object} yearData 
+     * @param {Integer} width 
+     * @param {Html element} svg 
+     */
+    createXAxis(yearData, width, height, svg) {
         // Create the X-Scale
-        let yearScale = d3.scaleBand()
+        this.xScale = d3.scaleBand()
             .domain(yearData)
             .range([this.padding.left, width - this.padding.right])
             .padding(.01);
 
-        this.xScale = yearScale;
-
-        let xaxis = d3.axisBottom(yearScale).ticks(8).tickFormat(d => {
+        let xaxis = d3.axisBottom(this.xScale).tickFormat(d => {
             return "" + d;
-        }).tickValues(yearScale.domain().filter(function(d,i){ return !(i%10)}))
+        }).tickValues(this.xScale.domain().filter(function(d,i){ return !(i%5)}))
         
         svg.append("g").attr("id", "x-axis")
             .attr("transform", "translate(0," + height + ")")
             .call(xaxis);
-        
-        // Create Y-Scale
-        let set_scale = d3.scaleBand()
-            .domain(setName)
-            .range([this.padding.top, height - this.padding.bottom])
-            .padding(.01);
-        
-        this.yScale = set_scale;
+    }
 
-        let max_color = parseInt(d3.max(data, d=> d.num_color));
-        let min_color = parseInt(d3.min(data, d=> d.num_color))
+     /**
+     * A helper method that creates the rectangle for the
+     * tooltip
+     */
+      createToolkit() {
+        let toolKit = d3.select("#svg_heatmap")
+            .append("g")
+            .attr("id", "#heat_tool_tip")
 
-        let colorScale = d3.scaleLinear()
-            .domain([min_color, max_color])
-            .range(['white', "#FFCF04"]);
-        
-        svg.append('g').attr('id', 'rect')
+        toolKit.append("rect")
+            .attr("id", "tooltip")
+            .attr("ry", 20)
+            .attr("rx", 20)
+            .style("opacity", 0)
+    }
+    
+    //#endregion
+
+
+    //#region DRAWING HEATMAP
+    
+    /**
+     * A helper function that will draw the Rect on 
+     * the heat map
+     * @param {Objecy} data 
+     * @param {Function} yearScale 
+     * @param {Function} set_scale 
+     * @param {Function} colorScale 
+     * @param {HTML element} svg 
+     */
+    drawRect(data, yearScale, set_scale, colorScale, svg) {
+        let rec = svg.append('g').attr('id', 'heat_rect')
             .selectAll('rect')
             .data(data)
             .enter()
             .append("rect")
-                .attr('x', d => yearScale(d.year))
-                .attr('y', d => set_scale(d.set_name))
-                .attr('width', yearScale.bandwidth())
-                .attr("height", set_scale.bandwidth())
-                .style("fill", d => {
-                    if(d.num_color == undefined)
-                        return 'gray'
-                    else
-                        return colorScale(parseInt(d.num_color))
-                })
-                .on("mouseover", (e,d) => this.mouseOverEvent(e,d))
-                .on("mousemove", (e,d) => this.mouseMoveEvent(e,d))
-                .on("mouseleave", (e,d) => this.mouseLeaveEvent(e,d));      
-        
-        this.createToolkit();
+            
+        rec.attr('x', d => yearScale(d.year))
+            .attr('y', d => set_scale(d.set_name))
+            .attr('width', yearScale.bandwidth())
+            .attr("height", set_scale.bandwidth())
+            .style("fill", d => {
+                if(d.num_color == undefined)
+                    return 'gray'
+                else
+                    return colorScale(parseInt(d.num_color))
+            })
+            .on("mouseover", (e,d) => this.mouseOverEvent(e,d))
+            .on("mousemove", (e,d) => this.mouseMoveEvent(e,d))
+            .on("mouseleave", (e,d) => this.mouseLeaveEvent(e,d));  
     }
-
-    /**
-     * A helper method that creates the rectangle for the
-     * tooltip
-     */
-     createToolkit() {
-        let toolKit = d3.select("svg_heatmap")
-            .append("g")
-            .attr("id", "#heat_tool_tip")
     
-        toolKit.append("rect")
-            .attr("class", "tool_tip")
-            .attr("ry", -20)
-            .attr("rx", -20)
-            .attr("height", 100)
-            .attr("width", 100)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-            .style("opacity", 0)
-    }
+    //#endregion
 
+    //#region Event Handlers
     mouseOverEvent(e,d) {
-        d3.select(".tool_tip")
-            .style("opacity", "80%")
-            .attr('x', this.xScale(d.year) + 30)
-            .attr('y', this.yScale(d.setName));
+        if(d.year != undefined && d.set_name != undefined) {
+            console.log(d.year)
 
-        d3.select("#heat_tool_tip")
-            .append("text")
-            .attr("id", "toolText")
-            .text("Set Name: " + d.set_name)
-            .attr('x', this.xScale(d.year) + 34)
-            .attr('y', this.yScale(d.set_name) + 20) 
-            .attr("font-weight", "bold")
-            .attr("font-size", 10)
+            let x = (this.xScale(d.year) < this.width-200)? 
+                this.xScale(d.year) : this.xScale(d.year)-200;
+            
+            let y = (this.yScale(d.set_name) > this.height - 100)? 
+                this.yScale(d.set_name)-100 : this.yScale(d.set_name);
 
-        d3.select("#heat_tool_tip").append("text")
-            .attr("id", "toolText")
-            .text("Number of Color: " + d.num_color)
-            .attr('x', this.xScale(d.year) + 34)
-            .attr('y', this.yScale(d.set_name) + 40) 
-            .attr("font-weight", "bold")
-            .attr("font-size", 10)
+            d3.select("#tooltip")
+                .style("opacity", "10%")
+                .attr("x", x + 30)
+                .attr("y", y + 10)
+                .attr("rx", 20)
+                .attr("ry", 20)
+                
+            d3.select("#heat_tool_tip")
+                .raise()
+                .append("text")
+                .attr("id", "toolText")
+                .text("Set Name: " + d.set_name)
+                .attr('x', x + 34)
+                .attr('y', y + 40) 
+
+
+            d3.select("#heat_tool_tip")
+                .raise()
+                .append("text")
+                .attr("id", "toolText")
+                .text("Number of Color: " + d.num_color)
+                .attr('x', x + 34)
+                .attr('y', y + 60) 
+        }
         
     }
 
     mouseMoveEvent(e,d) {
-        d3.select("#heat_tool_tip").style("opacity", "80%");
+        d3.select("#tooltip")
+            .style("opacity", "10%")
+            .attr("rx", 20)
+            .attr("ry", 20);
         
         d3.select("#heat_tool_tip").selectAll("text").style("opacity", 1);
 
     }
 
     mouseLeaveEvent(e,d) {
-        d3.select(".tool_tip")
+        d3.select("#tooltip")
                     .style("opacity", 0)
-                    .attr("x", "1000px")
-                    .attr("y", "1000px");
+                    .attr("x", 0)
+                    .attr("y", 0);
         
         d3.select("#heat_tool_tip").selectAll("text").remove()
     }
+
+    //#endregion
+
+    /**
+     * A helper function that will handle updating the table 
+     * based on the informaiton that is given.
+     */
+    updateHeatmap(data) {
+        let svg = d3.select("#svg_heatmap").attr("height", 700);
+        svg.append("g").attr("id", "heat_tool_tip");
+
+        this.height = parseInt(svg.style("height"))-this.padding.bottom;
+        this.width = parseInt(svg.style("width")) - this.padding.right;
+        
+        this.createScaling(data, this.height, this.width, svg);
+        this.drawRect(data, this.xScale, this.yScale, this.colorScale, svg);
+
+    }
+
 }
--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
     // One class (.js file) for each visualization
 // Call the drawX() functions for each of the visualizations
 
+
 data_list = [
             d3.csv('./data/inventories.csv'), // used
             d3.csv('./data/inventory_parts.csv'), // used
@@ -17,9 +18,9 @@ data_list = [
 Promise.all(data_list).then(data => 
     {
         data = processData(data);
+        data = data.filter(d => d.num_parts > 0);
         let heatmap_vis = new Heatmap(data);
         let squiggler = new TheSquiggler(data);
-
     }
 );
 
@@ -34,6 +35,7 @@ function processData(data) {
     let years = acendingYearData(data);
     let parts = d3.group(data[1], d => d.inventory_id);
     let group_inv = d3.group(data[0], d => d.set_num);
+    let color_group = d3.group(data[3], d => d.id);
     
     // Create a data structure to store the information
     let processData = [];
@@ -42,12 +44,14 @@ function processData(data) {
     // Adding the data to the years
     for( let [key, value] of years) {
         for (let index = 0; index < value.length; index++) {
+            [num_color, colors] = getNum_Colors(parts, group_inv, data[4], value[index].set_num, color_group);
             processData[pIndex++] = {
                 year: value[index].year,
                 set_name: value[index].name,
                 theme_name: getTheme_id(data[6], value[index]),
                 num_parts: parseInt(value[index].num_parts),
-                num_color: getNum_Colors(parts, group_inv, data[4], value[index].set_num)
+                num_color: num_color,
+                colors: colors
             };
         }
     }
@@ -87,7 +91,7 @@ function getTheme_id(themes, set) {
  * @param {Array} colors 
  * @param {Object} set 
  */
-function getNum_Colors(inv_part, inv, inv_set, set_num) {
+function getNum_Colors(inv_part, inv, inv_set, set_num, color_group) {
     let inv_id = inv.get(set_num);
     
     let unique_colors = new Set();
@@ -97,9 +101,10 @@ function getNum_Colors(inv_part, inv, inv_set, set_num) {
         if(parts !== undefined) {
             let colors = d3.group(parts, d => d.color_id);
             for([key, value] of colors) {
-                unique_colors.add(colors.get(key));
+                let item = color_group.get(value[0].color_id);
+                unique_colors.add(...item);
             }
         }
     }
-    return unique_colors.size;
+    return [unique_colors.size, [...unique_colors]];
 }
