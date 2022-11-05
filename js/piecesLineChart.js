8c5343cd0b5a07692181d04733d1e25418ebb8bd
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-05 17:07:29 -0600

    Tooltips now adjust with the log scale.

--- js/piecesLineChart.js
+++ js/piecesLineChart.js
@@ -44,19 +44,23 @@ class PiecesLineChart {
 
     }
 
+    //#region Draw Basic Chart
+
     drawLineChart() {
         console.log('AT: drawLineChart()');
 
-        this.drawAxes();
+        this.drawAxes(false);
         this.drawDots();
+        this.toggleLogScale();
     }
 
-    drawAxes() {
+    drawAxes(drawLogrithmic) {
         console.log('AT: drawAxes()');
 
         let svg = d3.select('#svg_piecesLineChart');
 
         // Draw Axes
+        console.log('this.svgHeight: ' + this.svgHeight)
 
         // Draw xAxis
         let xAxis = d3.axisBottom()
@@ -65,15 +69,24 @@ class PiecesLineChart {
                       .tickValues([1949, 1960, 1970, 1980, 1990, 2000, 2010, 2022])
 
         svg.append('g')
+           .attr('id', 'x-axis')
            .attr('transform', `translate(${0}, ${this.svgHeight - 25})`)
            .call(xAxis)
 
         // Draw yAxis
         let yAxis = d3.axisLeft()
                       .scale(this.yScale)
-                      .tickValues([0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000])
+        
+        if (drawLogrithmic) {
+            yAxis.tickValues([0, 10, 100, 1000, 10000, 12000]);
+        } else {
+            yAxis.tickValues([0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000]);
+        }
 
+
+        // TODO: A "<g> attribute trnasform: Trailing garbage, "translate(0,NaN)" error is being thrown near here when the log scale is toggled. It doesn't seem to be breaking anything, but fix it anyway.
         svg.append('g')
+           .attr('id', 'y-axis')
            .attr('transform', `translate(${45}, ${0})`)
            .call(yAxis)
     }
@@ -83,6 +96,7 @@ class PiecesLineChart {
 
         let svg = d3.select('#svg_piecesLineChart')
                     .append('g')
+                    .attr('id', 'dots-group')
 
         svg.selectAll('dot')
            .data(this.data)
@@ -98,14 +112,72 @@ class PiecesLineChart {
 
         this.applyToolTip();
     }
+
+    //#endregion
+
+    //#region Linear-to-Log Scale Change
+
+    // TODO: If time permits, add transistions to the log-to-linear scale transitions.
+    toggleLogScale() {
+
+        d3.select('#lineChart-log-toggle').on('click', e => {
+
+            console.log(e)
+
+            if (e.target.checked) {
+
+                console.log('HERE')
+
+                // TODO: Add more meaningful y-axis labels by passing true/false to the drawAxes() method to determine what axis labels to apply.
+
+                // Change yScale
+                this.yScale = d3.scaleLog()
+                                .domain([this.num_partsMin, Math.ceil(this.num_partsMax * 0.001) * 1000])
+                                .range([this.svgHeight - 25, 20])
+
+                // Remove old elements
+                d3.select('#dots-group').remove();
+                d3.select('#y-axis').remove();
+                d3.select('#piecesLCTooltip').remove();
+
+                // Redraw new elements
+                this.drawAxes(true);
+                this.drawDots();
+
+            } else {
+
+                // X-Scale
+                this.xScale = d3.scaleLinear()
+                                .domain([this.yearMin, this.yearMax]) // From
+                                .range([45, this.svgWidth - 20]) // To
+
+                // Y-Scale
+                this.yScale = d3.scaleLinear()
+                                .domain([this.num_partsMin, Math.ceil(this.num_partsMax * 0.001) * 1000])
+                                .range([this.svgHeight - 25, 20])
+
+                // Remove old elements
+                d3.select('#dots-group').remove();
+                d3.select('#y-axis').remove();
+                d3.select('#piecesLCTooltip').remove();
+
+                // Redraw new elements
+                this.drawAxes();
+                this.drawDots();
+
+            }
+
+        })
+    }
+
+    //#endregion
     
-    //#region Tooltip Test
+    //#region Tooltip Logic
 
     // TODO: Tooltips are getting cut off by the the rigth side and bottom of the SVG. Need to add checks to keep them from getting cut off.
 
     applyToolTip() {
         console.log('AT: applyToolTip()');
-        d3.select('#svg_piecesLineChart').append("g").attr("id", "piecesLCTooltip");
         this.createToolkit();
     }
 
@@ -116,7 +188,7 @@ class PiecesLineChart {
     createToolkit() {
         let toolKit = d3.select("#svg_piecesLineChart")
             .append("g")
-            .attr("id", "#piecesLCTooltip")
+            .attr("id", "piecesLCTooltip")
 
         toolKit.append("rect")
             .attr("id", "tooltip")
@@ -154,7 +226,7 @@ class PiecesLineChart {
                 .raise()
                 .append("text")
                 .attr("id", "toolText")
-                .text("Number of Color: " + d.num_parts)
+                .text("Number of Pieces: " + d.num_parts)
                 .attr('x', x + 34)
                 .attr('y', y + 60) 
         }
