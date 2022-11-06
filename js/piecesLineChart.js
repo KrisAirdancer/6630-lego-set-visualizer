123f6c9ab31ca4639943d8556a9ffb256b915be3
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-06 12:41:14 -0700

    Implemented constructor(), drawLineChart(), and drawAxes().

--- js/piecesLineChart.js
+++ js/piecesLineChart.js
@@ -1,7 +1,4 @@
 // Number of pieces per set per year
-
-// TODO: Consider adding a toggle to switch between number of themes per year and number of sets per year.
-
 class piecesLineChart {
     
 }
--- js/themesLineChart.js
+++ js/themesLineChart.js
@@ -1,5 +1,8 @@
 // Number of themes per year
 
+// TODO: Consider adding a toggle to switch between number of themes per year and number of sets per year or even both together.
+// TODO: Add tooltips
+
 class ThemesLineChart {
 
     constructor(data) { 
@@ -11,18 +14,67 @@ class ThemesLineChart {
 
         this.themesData = this.processData();
         // console.log(this.themesData)
-        
 
+        /* Get SVG Data */
+
+        this.svgHeight = parseInt(d3.select('#svg_themesLineChart').style('height'))
+        this.svgWidth = parseInt(d3.select('#svg_themesLineChart').style('width'))
+        console.log('svgHeight: ' + this.svgHeight)
+        console.log('svgWidth: '+ this.svgWidth)
+
+        /* Get Data Metadata */
+
+        this.yearMin = d3.min(this.themesData, d => d.year);
+        this.yearMax = d3.max(this.themesData, d => d.year);
+        this.num_themesMin = d3.min(this.themesData, d=> d.num_unique_themes);
+        this.num_themesMax = d3.max(this.themesData, d => d.num_unique_themes);
+        console.log('yearMin: ' + this.yearMin)
+        console.log('yearMax: '+ this.yearMax)
+        console.log('num_themesMin: ' + this.num_themesMin)
+        console.log('num_themesMax: '+ this.num_themesMax)
+
+        /* Make Scales */
+
+        // X-Scale
+        this.xScale = d3.scaleLinear()
+                        .domain([this.yearMin, this.yearMax]) // From
+                        .range([45, this.svgWidth - 20]) // To
+
+        // Y-Scale
+        this.yScale = d3.scaleLinear()
+                        .domain([this.num_themesMin, Math.ceil(this.num_themesMax * 0.1) * 10])
+                        .range([this.svgHeight - 25, 20])
     }
     
     //#region Draw basic chart
 
     drawLineChart() {
-
+        this.drawAxes();
+        this.drawLine();
     }
 
     drawAxes() {
+        let svg = d3.select('#svg_themesLineChart');
 
+        // Draw xAxis
+        let xAxis = d3.axisBottom()
+                      .scale(this.xScale)
+                      .tickFormat(d => `${d}`)
+                      .tickValues([1949, 1960, 1970, 1980, 1990, 2000, 2010, 2022])
+
+        svg.append('g')
+           .attr('id', 'x-axis')
+           .attr('transform', `translate(${0}, ${this.svgHeight - 25})`)
+           .call(xAxis)
+
+        // Draw yAxis
+        this.yAxis = d3.axisLeft()
+                      .scale(this.yScale)
+
+        svg.append('g')
+           .attr('id', 'y-axis')
+           .attr('transform', `translate(${45}, ${0})`)
+           .call(this.yAxis)
     }
 
     drawLine() {
