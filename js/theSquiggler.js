efa4305575d162474b9acce5985331311187bb75
Vista Marston <63620520+VistaMarston@users.noreply.github.com>
2022-11-09 16:52:46 -0700

    Added Line, Dots, and Year Labels to the path

--- js/theSquiggler.js
+++ js/theSquiggler.js
@@ -39,9 +39,6 @@ class TheSquiggler {
         this.height = parseInt(d3.select("#svg_theSquiggler").style("height"));
         this.width = parseInt(d3.select("#svg_theSquiggler").style("width"));
 
-        console.log(this.data)
-        console.log(d3.min(this.data, d => d.num_theme));
-
         let xScale = d3.scaleLinear()
                     .domain([0, d3.max(this.data, d => d.num_theme)])
                     .range([this.padding.left, this.width - this.padding.right]);
@@ -64,8 +61,57 @@ class TheSquiggler {
         
         svg.attr("transform", "translate("+ (this.padding.left+10) + ", 0)")
             .call(d3.axisLeft(yScale));
-        // Create an XScale -> num of themes
-        // Create an YScale -> average color/set
+        
+        svg = d3.select("#svg_theSquiggler")
+        .append("g")
+        .attr("id", "vis_path")
+
+        svg.append("path")
+            .datum(this.data)
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("d", d3.line()
+                .curve(d3.curveCardinal.tension(0.5))
+                .x(function(d) {
+                    console.log(d);
+                    return xScale(d.num_theme)
+                })
+                .y(d => yScale(d.avg_color)))
+
+        svg = d3.select("#svg_theSquiggler")
+            .append("g")
+            .attr("id", "vis_dots")
+        
+        svg.selectAll("dot")
+            .data(this.data)
+            .enter()
+            .append("circle")
+                .attr("cx", d => xScale(d.num_theme))
+                .attr("cy", d => yScale(d.avg_color))
+                .attr("r", 2)
+                .attr("stroke", "#69b3a2")
+                .attr("stroke-width", 3)
+                .attr("fill", "white")
+            
+        svg = d3.select("#svg_theSquiggler")
+            .append("g")
+            .attr("id", "vis_text")
+
+        svg.selectAll("text")
+            .data(this.data)
+            .enter()
+            .append("text")
+                .attr("x", d => xScale(d.num_theme) + 2)
+                .attr("y", d => yScale(d.avg_color) + 2)
+                .attr("stroke", "black")
+                .text(function(d,i) {
+                    if(i % 10 == 0)
+                        return d.year;
+                    return "";
+                })
+                .attr("font-size", 10)
+                .attr("opacity", "60%")
+                
         
     }
 
