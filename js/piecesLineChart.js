02dc47a10919f0c1c184da0be3aaa4f902ad62b9
Vista Marston <63620520+VistaMarston@users.noreply.github.com>
2022-11-18 11:12:39 -0700

    Added some animation and story in the piece line chart section of the code.

--- js/piecesLineChart.js
+++ js/piecesLineChart.js
@@ -2,18 +2,65 @@ class PiecesLineChart {
 
     constructor(data) {
         this.data = data;
+        this.clicked = false;
+        this.average = [];
+        let index = 0;
 
+        // Used to draw the mean line. 
+        let yearData = [...d3.group(data, d => d.year)];
+        yearData.forEach(year => {
+            // let avg = d3.quantile(year[1], .95, d => d.num_parts);
+            let avg = d3.mean(year[1], d => d.num_parts);
+            this.average[index++] = {
+                year: year[0],
+                piece: avg,
+            };
+        })
+
+
+        
         // Get SVG Data
+        d3.select('#svg_piecesLineChart').append("g").attr("id", "average_path");
+        let textBox = d3.select('#svg_piecesLineChart').append("g").attr("id", "text_box");
+        d3.select('#svg_piecesLineChart').append("g").attr("id", "histograms");
         this.svgHeight = parseInt(d3.select('#svg_piecesLineChart').style('height'))
         this.svgWidth = parseInt(d3.select('#svg_piecesLineChart').style('width'))
+        
+        textBox.append("text")
+                .attr("x", "200")
+                .attr("y", "200")
+                .attr("id", "explainBox")
+                .text("The mean line might not look like there is");
+        textBox.append("text")
+                .attr("x", "200")
+                .attr("y", "220")
+                .attr("id", "explainBox")
+                .text("a large difference between each year.")
+        textBox.append("text")
+                .attr("x", "200")
+                .attr("y", "240")
+                .attr("id", "explainBox")
+                .text("Click the log button to get a better view.")
 
         // Get Data Metadata
-
+        
         this.yearMin = d3.min(this.data, d => d.year);
         this.yearMax = d3.max(this.data, d => d.year);
         this.num_partsMin = d3.min(this.data, d=> d.num_parts);
         this.num_partsMax = d3.max(this.data, d => d.num_parts);
-
+        
+        // Create a data structure for the histograms
+        this.hisX1 = d3.scaleLinear()
+                .domain([this.yearMin, 1973])    
+                .range([0, this.svgWidth/3 - 10]);
+        
+        this.hisX2 = d3.scaleLinear()
+            .domain([1974, 1998])    
+            .range([this.svgWidth/3, 2*this.svgWidth/3 - 10]);
+        
+        this.hisX2 = d3.scaleLinear()
+            .domain([1999, this.yearMax])    
+            .range([2*this.svgWidth/3, this.svgWidth - 10]);
         // Make Scales
 
         // X-Scale
@@ -27,8 +74,36 @@ class PiecesLineChart {
                        .range([this.svgHeight - 25, 20])
 
         this.yAxis = undefined;
-
     }
+
+    //#region ANIMATE MEAN VALUE
+    draw_average() {
+        console.log(this.average);
+        let svg = d3.select('#svg_piecesLineChart').select("#average_path");
+
+        svg.selectAll("path").remove();
+        
+        var path = svg.append("path")
+            .datum(this.average)
+            .attr("d", d3.line()
+                .curve(d3.curveCardinal.tension(0.5))
+                .x(d => this.xScale(d.year))
+                .y(d => this.yScale(d.piece)))
+            .attr("stroke", "black")
+            .attr("stroke-width", "2")
+            .attr("fill", "none");
+
+        var totalLength = path.node().getTotalLength();
+
+        path.attr("stroke-dasharray", totalLength + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .transition()
+            .duration(4000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+            .on("end", d => d.year == "2022");
+    }
+    //#endregion
 
     //#region Draw Basic Chart
 
@@ -81,6 +156,8 @@ class PiecesLineChart {
            .attr("transform", "rotate(-90)")
            .text("Number of Pieces")
            .attr("font-size", 15)
+
+        this.draw_average()
         
     }
 
@@ -99,12 +176,7 @@ class PiecesLineChart {
            .attr('r', '2px')
            .style('fill', '#FFCF04')
            .style('stroke', 'black')
-           .style('stroke-width', '0.75px')
-           .on("mouseover", (e,d) => this.mouseOverEvent(e,d))
-           .on("mousemove", (e,d) => this.mouseMoveEvent(e,d))
-           .on("mouseleave", (e,d) => this.mouseLeaveEvent(e,d));  
-
-        this.applyToolTip();
+           .style('stroke-width', '0.75px');  
     }
 
     //#endregion
@@ -112,10 +184,32 @@ class PiecesLineChart {
     //#region Linear-to-Log Scale Change
 
     toggleLogScale() {
+        // d3.select('#svg_piecesLineChart').select("#text_box").remove();
 
         d3.select('#lineChart-log-toggle').on('click', e => {
-
+            // Change text and position for the log scale
             if (e.target.checked) {
+                d3.select('#svg_piecesLineChart').select("#text_box").remove();
+                let textBox = d3.select('#svg_piecesLineChart').append("g").attr("id", "text_box")
+
+                textBox.append("text")
+                    .attr("x", "100")
+                    .attr("y", "50")
+                    .attr("id", "explainBox")
+                    .style("opacity", "0")
+                    .text("There is a slight increase in the number")
+                    .transition()
+                    .duration(3000)
+                    .style("opacity", 1);
+                textBox.append("text")
+                    .attr("x", "100")
+                    .attr("y", "70")
+                    .attr("id", "explainBox")
+                    .style("opacity", "0")
+                    .text("of pieces with each year")
+                    .transition()
+                    .duration(3000)
+                    .style("opacity", 1);
 
                 // Change yScale
                 this.yScale = d3.scaleLog()
@@ -123,6 +217,37 @@ class PiecesLineChart {
                                 .range([this.svgHeight - 25, 20])
                                 .nice();
             } else {
+                d3.select('#svg_piecesLineChart').select("#text_box").remove();
+                let textBox = d3.select('#svg_piecesLineChart').append("g").attr("id", "text_box")
+
+                textBox.append("text")
+                    .attr("x", "100")
+                    .attr("y", "200")
+                    .attr("id", "explainBox")
+                    .style("opacity", "0")
+                    .text("The mean line might not look like there is")
+                    .transition()
+                    .duration(3000)
+                    .style("opacity", 1);
+                textBox.append("text")
+                    .attr("x", "100")
+                    .attr("y", "220")
+                    .attr("id", "explainBox")
+                    .style("opacity", "0")
+                    .text("a large difference between each year.")
+                    .transition()
+                    .duration(3000)
+                    .style("opacity", 1);
+                textBox.append("text")
+                    .attr("x", "100")
+                    .attr("y", "240")
+                    .attr("id", "explainBox")
+                    .style("opacity", "0")
+                    .text("Click the log button to get a better view.")
+                    .transition()
+                    .duration(3000)
+                    .style("opacity", 1);
+                // Change text and position 
 
                 // Y-Scale
                 this.yScale = d3.scaleLinear()
@@ -142,94 +267,15 @@ class PiecesLineChart {
                 .transition()
                 .duration(800)
                 .call(d3.axisLeft(this.yScale))
+
+            this.draw_average();
         })
+
     }
+    //#endregion
+
+    //#region TRANSFER TO HISTORGRAMS
 
     //#endregion
-    
-    //#region Tooltip Logic
 
-    applyToolTip() {
-        this.createToolkit();
-    }
-
-    /**
-     * A helper method that creates the rectangle for the
-     * tooltip
-     */
-    createToolkit() {
-        let toolKit = d3.select("#svg_piecesLineChart")
-            .append("g")
-            .attr("id", "piecesLCTooltip")
-
-        toolKit.append("rect")
-            .attr("id", "tooltip")
-            .attr("ry", 20)
-            .attr("rx", 20)
-            .style("opacity", 0)
-    }
-
-    mouseOverEvent(e,d) {
-        if(d.year != undefined && d.set_name != undefined) {
-
-            let x = (this.xScale(d.year) < this.svgWidth - 350)? 
-                this.xScale(d.year) : this.xScale(d.year) - 350;
-            
-            let y = (this.yScale(d.num_parts) > this.svgHeight - 100)? 
-                this.yScale(d.num_parts) - 100 : this.yScale(d.num_parts);
-
-            d3.select("#tooltip")
-                .style("opacity", "90%")
-                .attr("x", x + 30)
-                .attr("y", y + 10)
-                .attr("rx", 20)
-                .attr("ry", 20)
-                
-            d3.select("#piecesLCTooltip")
-                .raise()
-                .append("text")
-                .attr("id", "toolText")
-                .text("Set Name: " + d.set_name)
-                .attr('x', x + 34)
-                .attr('y', y + 40) 
-
-            d3.select("#piecesLCTooltip")
-                .raise()
-                .append("text")
-                .attr("id", "toolText")
-                .text("Number of Pieces: " + d.num_parts)
-                .attr('x', x + 34)
-                .attr('y', y + 60) 
-
-            d3.select("#piecesLCTooltip")
-                .raise()
-                .append("text")
-                .attr("id", "toolText")
-                .text("Year Published: " + d.year)
-                .attr('x', x + 34)
-                .attr('y', y + 80) 
-        }
-        
-    }
-
-    mouseMoveEvent(e,d) {
-        d3.select("#tooltip")
-            .style("opacity", "90%")
-            .attr("rx", 20)
-            .attr("ry", 20);
-        
-        d3.select("#piecesLCTooltip").selectAll("text").style("opacity", 1);
-
-    }
-
-    mouseLeaveEvent(e,d) {
-        d3.select("#tooltip")
-                    .style("opacity", 0)
-                    .attr("x", 0)
-                    .attr("y", 0);
-        
-        d3.select("#piecesLCTooltip").selectAll("text").remove()
-    }
-
-    //#endregion
 }
